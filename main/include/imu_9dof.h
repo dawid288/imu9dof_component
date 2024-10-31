3c5c687fc71f6ba7dfcb20fbf93ef13e77253054
Daw 1234 <daw23456764@gmail.com>
2024-10-31 08:51:53 +0100

    Add imu9dof library

---
+++ main/include/imu_9dof.h
@@ -0,0 +1,19 @@
+#ifndef IMU_9DOF_H
+#define IMU_9DOF_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <driver/i2c_master.h>
+#include "icm20600.h"
+#include "ak09918.h"
+
+#define ICM_20600_I2C_PORT 0
+#define ICM_20600_I2C_SDA  21
+#define ICM_20600_I2C_SDL  22
+
+#define I2C_FAST_MOOD 400000
+
+void imu_9dof_i2c_init(icm20600_dev_t* icm_dev, ak09918_dev_t* ak_dev);
+void imu_9dof_read(icm20600_dev_t* icm_dev, ak09918_dev_t* ak_dev);
+
+#endif
---
+++ main/main.c
@@ -0,0 +1,24 @@
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <driver/i2c_master.h>
+#include "ak09918.h"
+#include "icm20600.h"
+#include "imu_9dof.h"
+
+void app_main(void)
+{
+    icm20600_dev_t icm_dev1;
+    ak09918_dev_t ak_dev1;
+
+    imu_9dof_i2c_init(&icm_dev1, &ak_dev1);
+    icm20600_init(&icm_dev1);
+    ak09918_init(&ak_dev1);
+
+    while (1)
+    {
+        imu_9dof_read(&icm_dev1, &ak_dev1);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
